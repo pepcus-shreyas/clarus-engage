c6c01331daa8f5bc8030e6e7a90d4eda2250cadf
Shreyas Jain <shreyas.jain@pepcus.com>
2026-03-25 15:10:55 +0530

    CPT-8214: styling abstraction done

--- src/services/tasksService.js
+++ src/services/tasksService.js
@@ -1,6 +1,6 @@
-import { defaultHttpClient } from '@/app/lib/http'
+import { defaultHttpClient as apiClient } from '@/app/lib/http'
 
 /** @returns {Promise<{ data: unknown }>} */
 export function fetchTodos() {
-  return defaultHttpClient.get('/todos')
+  return apiClient.get('/todos')
 }
